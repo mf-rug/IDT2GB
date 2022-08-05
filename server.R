00adcbec1d0575f5396744196bc9cfac9702bf5e
mf-rug <max.fuerst@gmail.com>
2022-08-05 13:08:16 +0200

    final touch

--- server.R
+++ server.R
@@ -12,72 +12,85 @@ server <- function(input, output) {
   
   mods <- suppressWarnings(read_csv('all_IDT_modifications.csv', col_names = TRUE, show_col_types = FALSE) %>% as.data.frame())
   
-  processed <- eventReactive(c(input$seqs, input$remr), {
+  processed <- eventReactive(c(input$seqs, input$remr, input$remm, input$rems, input$remp, input$sep), {
     if (input$seqs != '') {
-      df <- read.table(text = input$seqs, sep = '\t')
-      df_mods <- df[,1:2]
-      df_mods$V2 <- str_remove(df_mods$V2, '^/')
-      df_mods$V2 <- str_replace_all(df_mods$V2, '//', '/')
-      df_mods <- suppressWarnings(separate(df_mods, 'V2', into = as.character(1:max(str_count(df_mods$V2, '/'))), sep = '/'))
-      df_mods[is.na(df_mods)] <- ''
-      
-      seqs <- str_replace_all(df$V2, '/[^/]+/', '')
-      print(paste('seqs found to be', paste(seqs, collapse = ',')))
-      
-      df_mods[apply(df_mods, 2, function(x) str_detect(x, '^[+*mrAGTCUN][+*mrAGTCUN]*$'))] <- ''
-      df_mods <- apply(df_mods, 1, function(x) c(x[x != ""], rep("", length(x[x == ""])))) %>% t() %>% as.data.frame()
-      if (input$remr) {
-        seqs <- str_remove_all(seqs, 'r')
-      }
-      if (input$remp) {
-        seqs <- str_remove_all(seqs, '[+]')
-      }
-      if (input$rems) {
-        seqs <- str_remove_all(seqs, '[*]')
-      }
-      if (input$remm) {
-        seqs <- str_remove_all(seqs, 'm')
-      }
-      df_mods$seqs <- seqs
-      
-      # remove empty columns
-      df_mods <- df_mods[,as.numeric(which(apply(df_mods,2, function(x) !all(str_detect(x, '^$')))))]
-      df_out <- df_mods[,c('V1', 'seqs')]
-      df_out$length <- str_count(df_out$seqs)
-      seqs_single_char_as_mod <- gsub('/[^/]+/', '€', df$V2)
-      if (any(str_detect(seqs_single_char_as_mod, '€'))) {
-        print('some mods found')
+      check <- str_split(input$seqs, '\n')[[1]]
+      check <- check[check != '']
+      if (str_count(input$seqs, input$sep) != length(check)) {
+        df_out <- '<i>Problem with parsing input. Not every line seems to contain the name-sequence separator only once.<br>Are you using the right separator (check options panel)?</i>'
+      } else {
+        df <- read.table(text = input$seqs, sep = ifelse(input$sep == '\\t', '\t', input$sep))
+        df_mods <- df[,1:2]
+        df_mods$V2 <- str_remove(df_mods$V2, '^/')
+        df_mods$V2 <- str_replace_all(df_mods$V2, '//', '/')
+        df_mods <- suppressWarnings(separate(df_mods, 'V2', into = as.character(1:max(str_count(df_mods$V2, '/'))), sep = '/'))
+        df_mods[is.na(df_mods)] <- ''
         
-        #this calculates position of the mod by compensating for the extra char that is the modification
-        #the first stays, but all others get subtracted by the number of mods that follow
-        pos <- lapply(str_locate_all(seqs_single_char_as_mod, '€'), function(x) as.numeric(x[,1]) - c(0, seq_len(max(0, length(x[,1]) -1))))
-        
-        #this corrects for the fact there is more positions for modifications than bases
-        #usually a mod is assigned to the next base, except the 3' end, which gets assigned to the last base
-        pos <- lapply(1:length(pos), function(x) if (!identical(pos[[x]], numeric(0))) {
-          c(pos[[x]][which(pos[[x]] != max(pos[[x]]) | pos[[x]] == 1)],
-            pos[[x]][which(pos[[x]] == max(pos[[x]]) & pos[[x]] != 1)] -1)
-        } else { NULL })
-        
-        df_rest <- df_mods[which(!colnames(df_mods) %in% c('V1', 'seqs'))]
-        for (i in 1:ncol(df_rest)) {
-          df_out[,paste0('mod_', i)] <- df_rest[, i]
-          lookup_mod <- mods[match(df_rest[,i],mods$code), 'name']
-          df_out[df_rest[,i] != "", paste0('mod_', i, '_name')] <- lookup_mod[!is.na(lookup_mod)]
-          lookup_pos <- sapply(pos, function(x) x[i]) %>% unlist()
-          df_out[df_rest[,i] != "", paste0('mod_', i, '_pos')] <- lookup_pos[!is.na(lookup_pos)]
+        seqs <- str_replace_all(df$V2, '/[^/]+/', '')
+        seq_chars <- str_split(seqs, '') %>% unlist() %>% unique()
+        if (!all(seq_chars %in% str_split(' +*mrAGTCUN', '')[[1]])) {
+          df_out <- paste("<i>Problem with parsing input. Unrecognised characters in sequences:", paste0(seq_chars[!seq_chars %in% str_split(' +*mrAGTCUN', '')[[1]]], collapse = ' '), "<br>The only allowed characters that are not within two '/' are ' +*mrAGTCUN'.</i>")
+        } else {
+          df_mods[apply(df_mods, 2, function(x) str_detect(x, '^[ +*mrAGTCUN][ +*mrAGTCUN]*$'))] <- ''
+          df_mods <- apply(df_mods, 1, function(x) c(x[x != ""], rep("", length(x[x == ""])))) %>% t() %>% as.data.frame()
+          seqs <- str_remove_all(seqs, ' ')
+          if (input$remr) {
+            seqs <- str_remove_all(seqs, 'r')
+          }
+          if (input$remp) {
+            seqs <- str_remove_all(seqs, '[+]')
+          }
+          if (input$rems) {
+            seqs <- str_remove_all(seqs, '[*]')
+          }
+          if (input$remm) {
+            seqs <- str_remove_all(seqs, 'm')
+          }
+          df_mods$seqs <- seqs
+          
+          # remove empty columns
+          df_mods <- df_mods[,as.numeric(which(apply(df_mods,2, function(x) !all(str_detect(x, '^$')))))]
+          df_out <- df_mods[,c('V1', 'seqs')]
+          df_out$length <- str_count(df_out$seqs)
+          seqs_single_char_as_mod <- gsub('/[^/]+/', '€', df$V2)
+          if (any(str_detect(seqs_single_char_as_mod, '€'))) {
+            print('some mods found')
+            
+            #this calculates position of the mod by compensating for the extra char that is the modification
+            #the first stays, but all others get subtracted by the number of mods that follow
+            pos <- lapply(str_locate_all(seqs_single_char_as_mod, '€'), function(x) as.numeric(x[,1]) - c(0, seq_len(max(0, length(x[,1]) -1))))
+            
+            #this corrects for the fact there is more positions for modifications than bases
+            #usually a mod is assigned to the next base, except the 3' end, which gets assigned to the last base
+            pos <- lapply(1:length(pos), function(x) if (!identical(pos[[x]], numeric(0))) {
+              c(pos[[x]][which(pos[[x]] != max(pos[[x]]) | pos[[x]] == 1)],
+                pos[[x]][which(pos[[x]] == max(pos[[x]]) & pos[[x]] != 1)] -1)
+            } else { NULL })
+            
+            df_rest <- df_mods[which(!colnames(df_mods) %in% c('V1', 'seqs'))]
+            for (i in 1:ncol(df_rest)) {
+              df_out[,paste0('mod_', i)] <- df_rest[, i]
+              lookup_mod <- mods[match(df_rest[,i],mods$code), 'name']
+              df_out[df_rest[,i] != "", paste0('mod_', i, '_name')] <- lookup_mod[!is.na(lookup_mod)]
+              lookup_pos <- sapply(pos, function(x) x[i]) %>% unlist()
+              df_out[df_rest[,i] != "", paste0('mod_', i, '_pos')] <- lookup_pos[!is.na(lookup_pos)]
+            }
+          }
+          colnames(df_out)[1:2] <- c("name", "sequence")
         }
       }
-      colnames(df_out)[1:2] <- c("name", "sequence")
       df_out
     }
   })
   
   out_text <- eventReactive(processed(), {
     if (input$seqs != '') {
-      df <- processed()
-      
-      bb_text <- paste0(
+      if (!is.data.frame(processed())) {
+        processed()
+      } else {
+        df <- processed()
+        
+        bb_text <- paste0(
         'LOCUS       ', df$name, '              ', df$length, ' bp    DNA     linear   ', toupper(format(Sys.Date(), format="%d-%b-%Y")), '
 FEATURES             Location/Qualifiers
      primer_bind     1..', df$length, '
@@ -98,21 +111,22 @@ PLACE_HOLDER_FOR_MODS
 ORIGIN      
         1 ', df$sequence, '
 //')
-      for (i in 1:nrow(df)) {
-        max_mods <- sum(!is.na(df[i, str_detect(colnames(df), '_pos$')]))
-        if (max_mods > 0) {
-          repl <- lapply(1:max_mods, function(x) paste('     Modified_base   ', 
-                                                       df[i, paste0('mod_', x, '_pos')], 
-                                                       '\n                     /label="', 
-                                                       df[i, paste0('mod_', x)],
-                                                       '"\n                     /Modification="', 
-                                                       df[i, paste0('mod_', x, '_name')])) %>% unlist() %>% paste0(collapse = '\n')
-        } else {
-          repl <- ''
+        for (i in 1:nrow(df)) {
+          max_mods <- sum(!is.na(df[i, str_detect(colnames(df), '_pos$')]))
+          if (max_mods > 0) {
+            repl <- lapply(1:max_mods, function(x) paste('     Modified_base   ', 
+                                                         df[i, paste0('mod_', x, '_pos')], 
+                                                         '\n                     /label="', 
+                                                         df[i, paste0('mod_', x)],
+                                                         '"\n                     /Modification="', 
+                                                         df[i, paste0('mod_', x, '_name')])) %>% unlist() %>% paste0(collapse = '\n')
+          } else {
+            repl <- ''
+          }
+          df[i, 'out'] <- str_replace(df[i, 'out'], 'PLACE_HOLDER_FOR_MODS', repl)
         }
-        df[i, 'out'] <- str_replace(df[i, 'out'], 'PLACE_HOLDER_FOR_MODS', repl)
+        df$out
       }
-      df$out
     }
   })
   
@@ -125,7 +139,9 @@ ORIGIN
   })
   
   output$outtable <- renderDataTable({
-    df <- processed()
+    if (is.data.frame(processed())) {
+      processed()
+    }
   })
   
   output$downloadgb <- downloadHandler(
--- ui.R
+++ ui.R
@@ -17,10 +17,14 @@ ui <- fluidPage(
       HTML('<strong><big>Input</strong></big>'), hr(),
       textAreaInput('seqs', label = NULL, placeholder = 'Enter names and sequences', height= '50vh'),
       HTML('<strong><big>Options</strong></big>'), hr(),
-      materialSwitch('remr', "Remove 'r' from RNA sequences", value = TRUE, right = TRUE, status = 'primary'),
-      materialSwitch('remp', "Remove '+' from LNA sequences", value = TRUE, right = TRUE, status = 'primary'),
-      materialSwitch('rems', "Remove '*' from phosphorothioate sequences", right = TRUE, value = TRUE, status = 'primary'),
-      materialSwitch('remm', "Remove 'm' from 2'-O-methyl sequences", right = TRUE, value = TRUE, status = 'primary')
+      fluidRow(column(12,
+                      div(style="display: inline-block;vertical-align:top",HTML("<strong>name-sequence separator:&nbsp</strong>")),
+                      div(style="display: inline-block;vertical-align:top",textInput('sep', NULL, value = '\\t', width = '100px'))
+                      )),
+      materialSwitch('remr', "remove 'r' from RNA sequences", value = TRUE, right = TRUE, status = 'primary'),
+      materialSwitch('remp', "remove '+' from LNA sequences", value = TRUE, right = TRUE, status = 'primary'),
+      materialSwitch('rems', "remove '*' from phosphorothioate sequences", right = TRUE, value = TRUE, status = 'primary'),
+      materialSwitch('remm', "remove 'm' from 2'-O-methyl sequences", right = TRUE, value = TRUE, status = 'primary')
     ),
     mainPanel(
       HTML('<br><strong><big>Output</strong></big>'), hr(),
